b6199b6c19873b35cf7e7aed980ce72647328206
muggezifter <gert@gertrietveld.com>
2018-01-19 12:32:12 +0000

    adjusted colors

--- node/argubot.js
+++ node/argubot.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec;
 const blessed = require('blessed');
 const contrib = require('blessed-contrib');
 const record = require('node-record-lpcm16');
-const { Models, Detector } = require("snowboy");
+const { Models, Detector } = require('snowboy');
 
 const antonyms = {
   ja : 'nee',
@@ -17,20 +17,27 @@ const antonyms = {
   wit : 'zwart'
 }
 
-
 const screen = blessed.screen({
   smartCSR: true
 });
 
 const cons = contrib.log({ 
+  label: 'CONSOLE',
   border: {
     type: 'line'
   },
-  label: 'CONSOLE',
   width: '80%',
   height: '15%',
   top: '70%',
-  left: 'center'
+  left: 'center',
+  style:{ 
+     border : {
+      fg: [0,250,0]
+     },
+     label : {
+      fg: [0,250,0]
+     }
+  }
 });
 
 const wave = contrib.line({ 
@@ -41,38 +48,45 @@ const wave = contrib.line({
   top: '10%',
   width: '80%',
   height: '60%',
+  padding: { top: 1, bottom: 0, left: 0, right: 0},
   left: 'center',
   numYLabels: 5,
   style:{ 
      line: [255,255,0],
-     text: [0,255,0],
-     baseline: [0,0,0]
+     baseline: [127,127,127],
+     border : {
+      fg: [0,250,0]
+     },
+     label : {
+      fg: [0,250,0]
+     }
   }
 })
 
 const create_bt = function(message) { 
-    return blessed.bigtext({
+  return blessed.bigtext({
     content: ' ' + message + ' ',
     border: { type: 'line' },
     padding: { top: 1, bottom: 1, left: 0, right:  2},
     width: 'shrink',
-    left: "center",
-    top: "center",
+    left: 'center',
+    top: 'center',
     height: 20,
+    shadow: true,
     style: {  
       bg: '#ff0000',
       fg: '#ffffff', 
       border: { fg: '#ffffff', bg: '#ff0000'}
-      }
-    });
-  }
+    }
+  });
+}
 
 screen.append(wave); 
 screen.append(cons);
 
 
 const arr_x = [...Array(100).keys()];
-const arr_y = arr_x.map(function(value, i){ return 127 + 127*Math.sin(value);});
+const arr_y = arr_x.map(function (value, i){ return 127 + 127*Math.sin(value);});
 
 wave.setData([ {
   x: arr_x,
@@ -91,7 +105,7 @@ for (key in antonyms) {
 }
 
 const detector = new Detector({
-  resource: "./node_modules/snowboy/resources/common.res",
+  resource: './node_modules/snowboy/resources/common.res',
   models: models,
   audioGain: 2.0
 });
@@ -102,7 +116,7 @@ var dots='';
 detector.on('silence', function () {
   if (++cnt_silence > 3 && ! argubot_is_speaking) {
     cons.log(' ARGUBOT luistert' + dots);
-    dots = (dots.length < 5)? dots+"." : "";
+    dots = (dots.length < 5)? dots+'.' : '';
     cnt_silence = 0;
   }
 });
@@ -127,17 +141,17 @@ detector.on('hotword', function (index, hotword, buffer) {
   if (! argubot_is_speaking) {
     cons.log(' U zegt ' + hotword);
     argubot_is_speaking = true;
-    var bt = create_bt(antonyms[hotword]);
+    const bt = create_bt(antonyms[hotword]);
     screen.append(bt);
     setTimeout(function(){ 
         bt.destroy();
-      },100);
-    exec('espeak -vnl+m2 ' + antonyms[hotword] , function callback(error, stdout, stderr){
+      },50);
+    exec('espeak -vnl+m2 ' + antonyms[hotword] , function (error, stdout, stderr){
       setTimeout(function(){ 
         argubot_is_speaking = false; 
-      },250);
+      },500);
     });
-    cons.log(" ARGUBOT zegt " +antonyms[hotword]);
+    cons.log(' ARGUBOT zegt ' + antonyms[hotword]);
   }
 });
 
